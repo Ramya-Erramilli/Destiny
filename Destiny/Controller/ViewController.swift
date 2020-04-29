d38b31c87135de39ce5f727ab4ece155d00e5074
Ramya Erramilli <e.ramya.103@gmail.com>
2020-04-29 18:45:00 +0530

    Final push

---
+++ Destiny/Controller/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+   var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+
+        updateUI()
+        
+        
+    }
+    
+    func updateUI(){
+        var selectedStory = storyBrain.getStory()
+        storyLabel.text = selectedStory.title
+        choice1Button.setTitle(selectedStory.choice1, for: .normal)
+        choice2Button.setTitle(selectedStory.choice2, for: .normal)
+        choice1Button.titleLabel?.sizeToFit()
+        choice2Button.titleLabel?.sizeToFit()
+    }
+    
+}
+
